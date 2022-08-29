b79d53b56859e54bf20472b023f70d5130cd7fab
make407 <make@nibss-plc.com.ng>
2022-08-29 10:29:35 +0100

    Script Modifications  cpay

--- checkout.testcpay.js
+++ checkout.testcpay.js
@@ -1,31 +1,11 @@
 /** @format */
 var sha256Script = document.createElement('script');
-sha256Script.setAttribute(
-  'src',
-  'https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.js'
-);
+sha256Script.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.js');
 document.head.appendChild(sha256Script);
 
 function CentralpayCheckout(data) {
-  const {
-    secretKey,
-    amount,
-    currency,
-    merchantId,
-    productDescription,
-    productId,
-    responseUrl,
-    transactionId,
-  } = data;
-  const dataToHash =
-    merchantId +
-    productId +
-    productDescription +
-    amount +
-    currency +
-    transactionId +
-    responseUrl +
-    secretKey;
+  const { secretKey, amount, currency, merchantId, productDescription, productId, responseUrl, transactionId } = data;
+  const dataToHash = merchantId + productId + productDescription + amount + currency + transactionId + responseUrl + secretKey;
   const hash = sha256(dataToHash);
   const payload = { ...data, hash };
 
@@ -36,36 +16,21 @@ function CentralpayCheckout(data) {
     return val;
   });
 
-  const iframe = document.createElement('iframe');
-  iframe.setAttribute(
-    'style',
-    'position:fixed;top:0;left:0;border:none;width:100%;height:100%;z-index:10000;'
-  );
-  iframe.setAttribute('id', `iframe`);
-  iframe.setAttribute('allowTransparency', 'true');
-  iframe.setAttribute('name', 'checkout');
   //========================================================================================
   /*                                                                                      *
    *                                      local host                                      *
    *                                                                                      */
   //========================================================================================
-  // iframe.setAttribute(
-  //   'src',
-  //   `http://localhost:3000/?q=${encodeURIComponent(stringifiedData)}`
-  // );
+  // const url = `http://localhost:3000/?q=${encodeURIComponent(stringifiedData)}`;
+  // window.location.replace(url);
 
- 
   //========================================================================================
   /*                                                                                      *
    *                                Test Environment                                      *
    *                                                                                      */
   //========================================================================================
-  iframe.setAttribute(
-    'src',
-    `https://test-centralpay2.nibss-plc.com.ng/?q=${encodeURIComponent(
-      stringifiedData
-    )}`
-  );
+  const url = `https://test-centralpay2.nibss-plc.com.ng/?q=${encodeURIComponent(stringifiedData)}`;
+  window.location.replace(url);
 
-  document.body.appendChild(iframe);
+ 
 }
