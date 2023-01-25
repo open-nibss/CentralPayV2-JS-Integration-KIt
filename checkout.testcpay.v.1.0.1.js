d18dfa76ffd4fcc2f2df8fbdfc16cb8359ea05e9
make407 <57134967+make407@users.noreply.github.com>
2023-01-25 16:33:58 +0100

    Fixes to v1.0.1

--- checkout.testcpay.v.1.0.1.js
+++ checkout.testcpay.v.1.0.1.js
@@ -1,20 +1,16 @@
-/** @format */
+  /** @format */
 var sha256Script = document.createElement('script');
 sha256Script.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.js');
 document.head.appendChild(sha256Script);
 
 function CentralpayCheckout(data) {
-  const { secretKey, amount, currency, merchantId, productDescription, productId, responseUrl, transactionId } = data;
+  const { secretKey, amount, currency, merchantId, productDescription, productId, responseUrl, transactionId, paymentMode } = data;
   const dataToHash = merchantId + productId + productDescription + amount + currency + transactionId + responseUrl + secretKey;
   const hash = sha256(dataToHash);
-  const payload = { ...data, hash };
+  const payload = { amount, currency, merchantId, productDescription, productId, responseUrl, transactionId,  hash };
 
-  const stringifiedData = JSON.stringify(payload, function (key, val) {
-    if (typeof val === 'function') {
-      return val.toString();
-    }
-    return val;
-  });
+  const stringifiedData = 'merchantid='+merchantId+'&amount='+amount+'&currency='+currency+'&productDescription='+encodeURIComponent(productDescription)+'&productId='+productId+'&responseUrl='+encodeURIComponent(responseUrl)+'&transactionId='+transactionId+'&paymentMode='+paymentMode+'&hash='+hash;
+
 
   //========================================================================================
   /*                                                                                      *
@@ -29,7 +25,7 @@ function CentralpayCheckout(data) {
    *                                Test Environment                                      *
    *                                                                                      */
   //========================================================================================
-  const url = `https://test-centralpay2.nibss-plc.com.ng/?q=${encodeURIComponent(stringifiedData)}`;
+  const url = `https://test-centralpay2.nibss-plc.com.ng/?${stringifiedData}`;
   window.location.replace(url);
 
   //========================================================================================
