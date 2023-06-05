735fd5d7cb3a081976a8c218009f3dd52ec1ef2b
tfalade423 <59825458+tfalade423@users.noreply.github.com>
2023-06-05 08:46:43 +0100

    Update checkout.testcpay.v.1.0.1.js

--- checkout.testcpay.v.1.0.1.js
+++ checkout.testcpay.v.1.0.1.js
@@ -1,15 +1,58 @@
-  /** @format */
+/** @format */
 var sha256Script = document.createElement('script');
 sha256Script.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.js');
 document.head.appendChild(sha256Script);
 
 function CentralpayCheckout(data) {
-  const { secretKey, amount, currency, merchantId, productDescription, productId, responseUrl, transactionId, paymentMode } = data;
+  const { secretKey, amount, currency, merchantId, productDescription, productId, responseUrl, transactionId, paymentMode, logo } = data;
   const dataToHash = merchantId + productId + productDescription + amount + currency + transactionId + responseUrl + secretKey;
   const hash = sha256(dataToHash);
-  const payload = { amount, currency, merchantId, productDescription, productId, responseUrl, transactionId,  hash };
 
-  const stringifiedData = 'merchantid='+merchantId+'&amount='+amount+'&currency='+currency+'&productDescription='+encodeURIComponent(productDescription)+'&productId='+productId+'&responseUrl='+encodeURIComponent(responseUrl)+'&transactionId='+transactionId+'&paymentMode='+paymentMode+'&hash='+hash;
+  const payload = { ...data, hash };
+  const stringifiedData = JSON.stringify(payload, function (key, val) {
+    if (typeof val === 'function') {
+      return val.toString();
+    }
+    return val;
+  });
+
+  const constructUrl = () => {
+    let url = ""
+    if (merchantId) {
+      url += `merchantid=${merchantId}`
+    }
+    if (productId) {
+      url += `&productId=${productId}`
+    }
+    if (productDescription) {
+      const encodedProductDescription = encodeURIComponent(productDescription);
+      url += `&productDescription=${encodedProductDescription}`;
+    }
+    if (amount) {
+      url += `&amount=${amount}`
+    }
+    if (currency) {
+      url += `&currency=${currency}`
+    }
+    if (transactionId) {
+      url += `&transactionId=${transactionId}`
+    }
+    if (responseUrl) {
+      url += `&responseUrl=${responseUrl}`;
+    }
+    if (hash) {
+      url += `&hash=${hash}`
+    }
+    if (logo) {
+      url += `&logo=${logo}`
+    }
+    if (paymentMode) {
+      const formattedPaymentMode = paymentMode.replace(/ /g, '%20')
+      url += `&paymentMode=${formattedPaymentMode}`;
+    }
+
+    return url
+  }
 
 
   //========================================================================================
@@ -17,22 +60,22 @@ function CentralpayCheckout(data) {
    *                                      local host                                      *
    *                                                                                      */
   //========================================================================================
-  // const url = `http://localhost:3000/?q=${encodeURIComponent(stringifiedData)}`;
+  // const url = `http://localhost:3000/?${constructUrl()}`;
   // window.location.replace(url);
-  
   //========================================================================================
   /*                                                                                      *
    *                                Test Environment                                      *
    *                                                                                      */
   //========================================================================================
-  const url = `https://test-centralpay2.nibss-plc.com.ng/?${stringifiedData}`;
-  window.location.replace(url);
+  // const url = `https://test-centralpay2.nibss-plc.com.ng/?${constructUrl()}`
+  // window.location.replace(url);
+
 
   //========================================================================================
   /*                                                                                      *
    *                                Production Environment                                *
    *                                                                                      */
   //========================================================================================
-  // const url = `https://centralpay2.nibss-plc.com.ng/?q=${encodeURIComponent(stringifiedData)}`;
-  // window.location.replace(url);
+  const url = `https://centralpay2.nibss-plc.com.ng/?${constructUrl()}`;
+  window.location.replace(url);
 }
